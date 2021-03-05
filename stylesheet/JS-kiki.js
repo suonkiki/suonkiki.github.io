20e6a64d18fcf658c0a680b165be02070b61ed39
suonkiki <80063806+suonkiki@users.noreply.github.com>
2021-03-04 22:44:03 -0500

    kiki

---
+++ stylesheet/JS-kiki.js
@@ -0,0 +1,18 @@
+document.addEventlistener('DOMContentloaded',()=>{
+const timeleftDisplay=document.querySelector('#time-left')
+const starBtn=document.querySelector('#start-button')
+Left timeleft=10
+
+ var circle=document.getElementById("cricle")
+function oneFunction() {
+  var greeting;
+  var time = new Date().getHours();
+  
+ 
+  document.getElementById("demo").innerHTML = d;
+  function myFunction()
+{
+	x=document.getElementById("demo");  // 
+	x.innerHTML="Hello Burger king!";    // 
+}
+
